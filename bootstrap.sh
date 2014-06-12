e4261613c4536bbdf9eefb55916e27586fdaba59
Ryan <schu@ctrip.com>
2014-06-12 15:09:38 +0800

    remove git-completion downloading.

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,9 +5,6 @@ cd "$(dirname "${BASH_SOURCE}")"
 # update
 git pull origin master
 
-# get git-completion script from git repo on github.
-curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o ./.scripts/git-completion.bash
-
 function doIt() {
 	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
 		--exclude "readme.md" -avh --no-perms . ~
